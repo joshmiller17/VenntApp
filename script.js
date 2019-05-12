d3bebf15f9f3f354944fd6aa65ce37d38504c859
Josh Miller <miller.josh@husky.neu.edu>
2019-05-12 15:13:10 -0400

    shuffle deck

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 var debug = true;
 
+/**
+ * Randomly shuffle an array
+ * from https://stackoverflow.com/a/2450976/1293256
+ * @param  {Array} array The array to shuffle
+ * @return {String}      The first item in the shuffled array
+ */
+function shuffle(array) {
+
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
 function updatePiles(){
 	deck = document.getElementById("deckButton"); 
     deck.innerHTML = `Deck (${character.deck.length})`;
@@ -30,7 +56,6 @@ character.currentDeck = character.deck; /* Reset deck, will eventually want to c
 }
 
 
-
 function toggle_visibility_by_class(cl, state){ /* state is usually none or block */
  var divs = document.getElementsByClassName(cl);
     for(var i = 0; i < divs.length; i++){
@@ -46,7 +71,7 @@ function drawCard(card){
 		/* FIXME security issue with setting the HTML directly from the cards.
 		Need to sanitize inputs here. */
 		//TODO remove debug border
-		cardText = `<div style="vertical-align:top;text-align:left;
+		cardText = `<div style="display:block;vertical-align:top;text-align:left;
 					font-weight:bold;font-size:12px;border-style:solid">
 		<b>${card.name} (${card.cost}/${card.power})</b>
 		<br>
@@ -137,5 +162,6 @@ character.deck.push(new Card("Test Card B", "Red", ["Vigor", "Ranged"], 1, 1, "T
 character.deck.push(new Card("Test Card C", "Blue", ["Mental"], 1, 2, "This card does C"));
 /* Go to the Battle page on startup */
 deckPile = character.deck;
+shuffle(deckPile);
 battleClick();
 updatePiles();
