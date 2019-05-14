94c2f7f533dbeda5133cf6286cd57c3042b05ff9
Josh Miller <miller.josh@husky.neu.edu>
2019-05-13 23:32:02 -0400

    wip: load cards

--- script.js
+++ script.js
@@ -3,6 +3,102 @@ var cards = {}; //map of id : Card
 var ALL_CARDS = []
 var idCounter = 0;
 
+//FIXME this is janky
+var cardText = `
+
+Path - Beginner
+
+Endure (0/1)
+Type: Vigor
+Dodge. Heal 1 HP.
+
+Vigilance (0/1)
+Type: Mental
+Choose a target: reveal their intended actions. Play 1.	 
+
+Sprint (0/1)
+Type: Vigor, Move
+Dodge. Move up to [AGI] + [STA]. Flush.
+
+
+Defensive Strike (0/1)
+Type: Melee
+Dodge. [DEX] + 2 dmg.
+
+Last Breath (0/1)
+Type: Vigor
+Dodge. Deal dmg equal to twice the number of cards in your Flush pile. Flush.
+
+Running Fist (0/1)
+Type: Melee, Move
+Move up to 4, then:
+[STR] + 2 dmg.
+
+Rolling Shot (0/1)
+Type: Ranged, Move
+Dodge. Move 1. [DEX] dmg.
+
+Coordinate (0/1)
+Type: Social
+Choose a target. They take +2 dmg from all attacks this turn.
+
+Rally (0/1)
+Type: Social
+Recurring. You and adjacent allies deal +1 damage until the end of the turn.
+
+Calculated Advantage (1/1)
+Type: Melee
+([INT]x3) - ([Round]x2) dmg.
+
+Straight Shot (1/1)
+Type: Ranged
+Deal dmg equal to [PER] + [Round] + distance from target (max. 6)
+
+Charge (1/1)
+Type: Melee, Move
+Move up to 6. Deal damage equal to the number of squares you moved this turn (“move anywhere” abilities count as 0).
+
+Flash of Insight (1/1)
+Type: Ranged
+Dodge. [INT] + ([Round]x2) dmg.
+
+Fighting Spirit (1/1)
+Type: Vigor
+Dodge. Heal 4 HP. Melee [SPI] dmg.
+
+Rapid Fire (1/1)
+Type: Ranged
+Deal [PER] dmg, three times.
+
+Pummel (1/1)
+Type: Melee
+Deal [DEX] dmg a number of times equal to the number of times this target was hit this Round.
+
+Spinning Attack (1/1)
+Type: Melee
+Radius 1. [STR] + [Round] dmg.
+
+Vengeful Spirit (1/1)
+Type: Magic
+Deal dmg equal to [SPI] + [Round] + the amount of HP you have currently lost (from your maximum amount).
+
+Charged Shot  (1/1)
+Type: Magic
+Recurring. Add one counter to this card. Treat this card as an unplayable Basic. You may discard this card at any time to deal [DEX] x [number of counters] damage.
+
+Barrel Blast (2/2)
+Type: Alch
+Radius 3. [WIS] + [Round] dmg.
+
+Army of One (2/2)
+Type: Melee
+Radius 1. Deal [STA] + ([Round]x2) dmg twice.
+
+All In (2/1)
+Type: Vigor
+Draw 2, Play 2. Next turn, do not draw a new hand.
+`;
+
 /**
  * Randomly shuffle an array
  * from https://stackoverflow.com/a/2450976/1293256
@@ -75,6 +171,40 @@ function toggle_visibility_by_class(cl, state){ /* state is usually none or bloc
     for(var i = 0; i < divs.length; i++){
         divs[i].style.display = state;
     }
+}
+
+// //WIP
+// function cardShop(card){
+	// //TODO this needs some work, it messes a few things up
+	// var button = document.createElement("button");
+	// button.classList.add("button");
+	// button.classList.add("card_button");
+	// button.onclick = addCardToDeck;
+	// //button.oncontextmenu = removeCardFromDeck;
+	// button.id = generateId();
+	// cards[id] = card;
+	// /* FIXME security issue with setting the HTML directly from the cards.
+	// Need to sanitize inputs here. */
+	// //TODO remove debug border once the formatting looks good
+	// cardText = `<div style="display:block;vertical-align:top;text-align:left;
+				// font-weight:bold;font-size:12px;border-style:solid">
+	// <b>${card.name} (${card.cost}/${card.power})</b>
+	// <br>
+	// ${card.types}
+	// <br><br>
+	// ${card.effect}
+	// </div>`;
+	// button.innerHTML = cardText;
+	// button.style.display = "block";
+	// shop = document.getElementById("shop");
+	// shop.appendChild(button);
+// }
+
+function addCardToDeck(clickEvent){
+	cardButton = clickEvent.currentTarget;
+	id = cardButton.id;
+	deckPile.push(id);
+	updatePiles();
 }
 
 function drawCard(id){
@@ -185,21 +315,57 @@ function statMod(param, delta){
 	document.getElementById(eleName).innerHTML = param.toUpperCase() + ": " + character.stats[param]
 }
 
-function loadCards(cardfile){
+function loadCards(){
+	lines = cardText.split('\n');
+	var path = "";
+	var name = "";
+	var cost = -1;
+	var power = -1;
+	var cp_regex = /(\d+)\/(\d+)\)/;
+	var types = [];
+	var effect = "";
+	for (var i=0; i < lines.length; i++){
+		if (lines[i].startsWith("Path - ")) {
+			path = lines[i].substring(7);
+		}
+		else if (lines[i].length > 0) {
+			if (name == "") {
+				var splithead = lines[i].split(" (");
+				if (splithead.length < 2) {
+						console.log("Bad card: " + lines[i]);
+						continue;
+				}
+				name = splithead[0];
+				var match = splithead[1].match(cp_regex);
+				cost = match[1];
+				power = match[2];
+				if (cost == "undefined" || power == "undefined"){
+					console.log("Bad card: " + lines[i]);
+					continue;
+				}
+			}
+			else if (types == "") {
+				types = lines[i].substring(6).split(", ");
+			}
+			else {
+				effect += lines[i];
+			}
+		}
+		else { // end of card
+			if (name != "") {
+				card = new Card(name, path, types, cost, power, effect);
+				ALL_CARDS.push(card);
+			}
+			name = "";
+			types = [];
+			effect = "";
+		}
+	}
 	
-	return null;
-	// TODO
-	
-		// lines = data.split('\n');
-		// var path = "";
-		// for (var i=0; i < lines.length; i++){
-			// if (lines[i].startsWith("Path - ")) {
-				// path = lines[i].substring(7);
-				// console.log("Path: " + path) //TODO delete
-			// }
-			// // TODO add logic to read in cards
-		// }
-	// });
+	//WIP
+	// for (card in ALL_CARDS) {
+		// cardShop(ALL_CARDS[card]);
+	// }
 }
 
 class Path {
@@ -261,5 +427,5 @@ for (card in character.deck) {
 }
 shuffle(deckPile);
 battleClick();
-loadCards("cards.txt");
+loadCards();
 updatePiles();
